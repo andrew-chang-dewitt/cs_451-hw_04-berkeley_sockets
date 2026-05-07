030330d75d0eb3435f11bf281ca2f257fa6e0cb1
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2026-05-07 02:37:35 -0500

    chore(deploy): incomplete deploy scripting

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 """Set up FABRIC nodes for assignment."""
 
+from sys import exit
+
 from fabrictestbed_extensions.fablib.fablib import FablibManager
 from fabrictestbed_extensions.fablib.node import Interface, Node
 
@@ -14,9 +16,14 @@ hw_04 = fablib.new_slice(name=slice_name)
 nodes: list[Node] = []
 nics: list[Interface] = []
 
-def init_node(name:str) -> None:
-    node = hw_04.add_node(name=name,image=image,cores=2,ram=4,disk=9,site=site)
-    nic = node.add_component(model="NIC_Basic",name="iface1").get_interfaces()[0]
+
+def init_node(name: str) -> None:
+    node = hw_04.add_node(name=name, image=image, cores=2, ram=4, disk=9, site=site)
+    try:
+        nic = node.add_component(model="NIC_Basic", name="iface1").get_interfaces()[0]  # type: ignore
+    except KeyError:
+        print("expected nics to be list!")
+        exit(1)
 
     nodes.append(node)
     nics.append(nic)
@@ -24,9 +31,9 @@ def init_node(name:str) -> None:
 
 init_node("main")
 
-for i in range(1,11):
+for i in range(1, 11):
     init_node(f"child{i}")
 
-net = hw_04.add_l2network(name="net",interfaces=nics)
+net = hw_04.add_l2network(name="net", interfaces=nics)
 
 hw_04.submit()
